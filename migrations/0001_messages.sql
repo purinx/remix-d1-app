314bfb6bde0905434758b68e0dcecb0c455ebe79
higherkingpud <eddix.ibanez@outlook.jp>
2024-10-28 15:35:14 +0900

    add loader

--- migrations/0001_messages.sql
+++ migrations/0001_messages.sql
@@ -1,3 +1,4 @@
+-- Migration number: 0001 	 2024-10-28T05:08:36.087Z
 CREATE TABLE `messages` (
 	`id` integer PRIMARY KEY NOT NULL,
 	`from` text NOT NULL,
---
+++ scripts/seed.sql
@@ -0,0 +1,1 @@
+INSERT INTO messages (`from`, `body`) values ("purinx", "Hello, Cloudflare D1!");
